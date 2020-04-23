c29c108d16df179739f937fcf7ef2893f226d6db
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-04-23 19:05:56 +0700

    answer1

--- answer_1.php
+++ answer_1.php
@@ -1,8 +1,18 @@
 <?php 
-$a = 8;
-$b = 10;
+echo "8. 8 < 10 = "; var_dump(8<10); // bool(false)
+echo "<br />";
+ 
+echo " 8 <= 6 = "; var_dump(8<=6); // bool(true) 
+echo "<br />";
+ 
+echo "8 <> '30' = "; var_dump(8>'30'); // bool(false) 
+echo "<br />";
+ 
+echo "8. 10 == '10' = "; var_dump(8 =='10'); // bool(true) 
+echo "<br />";
+ 
+echo "8.  11 === '11' = "; var_dump(11 ==='11'); // bool(false) 
+echo "<br />";
+ 
 
-echo $a + $b;
 ?>
-
-
