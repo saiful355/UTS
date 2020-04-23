4b62949c958d8c5100622dcc3c6bf722dd6e05fc
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-04-23 19:18:15 +0700

    answer2

---
+++ answer_2.php
@@ -0,0 +1,11 @@
+<h1> Variable content </h1>
+
+<?php
+$nama= 'Saiful Bahri';
+?>Nama Saya : <?php echo $nama;?> <br>
+Variabel nama Saya : <?php var_dump($nama);?>
+<br>
+<?php
+$umur= '28';
+?>Umur Saya : <?php echo $umur;?> <br>
+Variabel umur Saya : <?php var_dump($umur);?>
